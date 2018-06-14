654fe07f189218e626d6425b8108386a7b5e6f4b
matheusladislau <matheusladislauribeiro@gmail.com>
2018-06-14 00:14:08 -0300

    alterado número de épocas

--- model.py
+++ model.py
@@ -15,15 +15,16 @@ y=dataset[:,28]
 from keras.utils import to_categorical
 y_binary = to_categorical(y)
 
-print(y_binary)
-
 model=Sequential()
 model.add(Dense(56,input_dim=28,activation='relu'))
 model.add(Dense(2,activation='softmax'))
 model.compile(optimizer='sgd',loss='categorical_crossentropy',metrics=['acc'])
-model.fit(x,y_binary,epochs=1000);
+model.fit(x,y_binary,epochs=2500);
 
-i=[0, 0, 0,0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+#caso jogo tenha 2 generos em comum com que usuário costuma jogar
+i=[0, 0, 0,1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 t=np.asmatrix(i);
-result=model.predict(t);
-print(i,"  prevê = ",result);
+resultado=model.predict(t);
+print(i)
+print("chance de gostar do jogo:",resultado[0][1]*100,"%")
+
